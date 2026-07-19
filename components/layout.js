7d5e0157f12c0a07401d13298cad4a25fff117c4
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-07-19 20:38:12 +0700

    setup

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Fauzan Husain</title>
+        <title>Startup</title>
       </Head>
       <main className={`${poppins.className}`}>{children}</main>
     </>
