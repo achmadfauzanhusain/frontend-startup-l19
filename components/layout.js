2bdc8dedd3b7c7d87ab71fea377647c5b3307ed2
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-07-19 20:20:44 +0700

    first commit

---
+++ components/layout.js
@@ -0,0 +1,18 @@
+import { Poppins } from 'next/font/google'
+import Head from 'next/head'
+
+const poppins = Poppins({ 
+  subsets: ['latin'], 
+  weight: ['200', '400', '600', '800']
+})
+ 
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Fauzan Husain</title>
+      </Head>
+      <main className={`${poppins.className}`}>{children}</main>
+    </>
+  )
+}
