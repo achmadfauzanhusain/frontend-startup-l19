e72ed3cdea4bbe6e79803760ca1bf0d33a535353
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-09-07 09:01:15 +0700

    api(server)

--- pages/server/index.js
+++ pages/server/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import ConnectWallet from "@/components/connectWallet";
 
 const Server = () => {
+    
     return(
         <div className="flex flex-col gap-2 md:flex-row">
             <div className="w-full md:w-2/3 border-0 md:border-r px-2">
---
+++ services/server.js
@@ -0,0 +1,48 @@
+import callApi from "@/lib/api";
+
+const ROOT_API = "http://localhost:8000"
+
+export async function createServer(data) {
+    const url = `${ROOT_API}/server/create`
+    return callApi({
+        url,
+        method: "POST",
+        data,
+        token: true
+    })
+}
+
+export async function getAllServer() {
+    const url = `${ROOT_API}/server/all`
+    return callApi({
+        url,
+        method: "GET",
+    })
+}
+
+export async function getMyServer() {
+    const url = `${ROOT_API}/server/my/server`
+    return callApi({
+        url,
+        method: "GET",
+        token: true
+    })
+}
+
+export async function getDetailServer(idServer) {
+    const url = `${ROOT_API}/server/${idServer}`
+    return callApi({
+        url,
+        method: "GET",
+        token: true
+    })
+}
+
+export async function getDetailServer(hashAddress) {
+    const url = `${ROOT_API}/server/joined/${hashAddress}`
+    return callApi({
+        url,
+        method: "GET",
+        token: true
+    })
+}
